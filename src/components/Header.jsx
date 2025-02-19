4e5120a686ce2c53076f61a18a86d8a84693eb8a
ItzSanket99 <161996340+ItzSanket99@users.noreply.github.com>
2025-02-19 08:08:53 +0530

    added navigation bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import logo from '../assets/image/logo.png'
+import { Link } from 'react-router-dom'
+import '../styles/Header.scss'
 
 const Header = () => {
   return (
     <nav>
       <img src={logo} alt="Logo" />
-      
+      <main>
+        <Link to={"/"} >Home</Link>
+        <Link to={"/#about"} >About Us</Link>
+        <Link to={"/services"} >Services</Link>
+        <Link to={"/#contact"} >Contact me</Link> 
+      </main>
+      <button>Login</button>
     </nav>
   )
 }
