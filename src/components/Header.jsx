1a8a8daf83b9e550efeb8b675a1f217caba48701
ItzSanket99 <161996340+ItzSanket99@users.noreply.github.com>
2025-02-19 07:20:11 +0530

    commit assets

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../assets/image/logo.png'
+
+const Header = () => {
+  return (
+    <nav>
+      <img src={logo} alt="Logo" />
+      
+    </nav>
+  )
+}
+
+export default Header;
